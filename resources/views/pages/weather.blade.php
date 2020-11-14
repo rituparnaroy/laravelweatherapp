77e5b51314820fe04ed40e7082633c68574a9ca1
Rituparna <rituparna.june@gmail.com>
2020-11-14 23:46:19 +0100

    blade file changes

--- resources/views/pages/weather.blade.php
+++ resources/views/pages/weather.blade.php
@@ -7,14 +7,14 @@
                         @csrf
                         <div class="form-group">
                             <label for="city" class="control-label col-sm-2">City:</label>
-                            <input type="text" name="city" placeholder="Enter city" class="@error('city') is-invalid @enderror col-sm-10" />
+                            <input type="text" name="city" placeholder="Enter city" class="@error('city') is-invalid @enderror col-sm-5" />
                             @error('city')
                                 <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="form-group">
                             <label for="countrycode" class="control-label col-sm-2">Country code:</label>
-                            <input type="text" name="countrycode" placeholder="Enter 2-letter country code" class="@error('countrycode') is-invalid @enderror col-sm-10" />
+                            <input type="text" name="countrycode" placeholder="Enter 2-letter country code" class="@error('countrycode') is-invalid @enderror col-sm-5" />
                             @error('countrycode')
                                 <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
@@ -23,17 +23,15 @@
                         <button type="submit" class="btn btn-default btn-success">Submit</button>
                     </form>
                 </div>
+                @if (!empty($avgTemperature)) 
+                    <div class="col-12 ">
+                        <h3>Average temperature of {{ $city }}, {{ $country }} for next 10 days is <mark> {{ $avgTemperature ?? '' }} </mark>&deg;c </h3>
+                    </div>
+            @endif
             </div>
+            
         </div>
-    @if (!empty($avgTemperature)) 
-        <div class="container">
-            <div class="row row-content">
-                <div class="col-12">
-                    Average temperature of {{ $city }}, {{ $country }} for next 10 days is {{ $avgTemperature ?? '' }} &deg;c
-                </div>
-            </div>    
-        </div>
-    @endif
+
     @if (!empty($message)) 
         <div class="container">
             <div class="row row-content">

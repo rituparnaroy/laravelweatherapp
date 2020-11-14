64343d56908e86b3607758813f4c7c27b726ce56
Rituparna <rituparna.june@gmail.com>
2020-11-15 00:01:59 +0100

    Header title change

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row row-header">
         <div class="col-12 col-sm-6">
-            <h1>Laravel Weather App</h1>
+            <h1>Wonderkind Weather App</h1>
         </div>
     </div>        
 </div>
